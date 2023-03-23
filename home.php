9e3b81c8025a113d280f702d1aa68e45f32ca38a
Marlou Reyes <69040884+marloureyes@users.noreply.github.com>
2023-03-23 16:50:52 +0800

    My initial push.

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Template Name: Home
+*/
+
+// Your custom code for the home page goes here
+
+get_header();
+?>
+
+
+<main id="primary" class="site-main">
+
+ hey
+</main><!-- #main -->
